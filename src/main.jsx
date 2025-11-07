2e34a37f3afe417da3ab20c18ffeed81c6467503
SumitCodeRepo <sumitharit1410@gmail.com>
2025-11-07 14:16:03 +0530

    Demo

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+console.log("TEST VALUE:", import.meta.env.VITE_TEST_VALUE);
+if (!PUBLISHABLE_KEY) {
+  console.log("Clerk key:", PUBLISHABLE_KEY);
+  throw new Error('Missing Publishable Key')
+}
+
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
             <Provider store={store}>
